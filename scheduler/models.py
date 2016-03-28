7b1120a2f390a8dc08a993ed65a80d62d2e2fbb6
lirothen <lirothen@gmail.com>
2016-03-28 19:43:02 -0400

    changed usershortlist model

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,21 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    fb_userid = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    profile_img_url = models.CharField(max_length=500)
-
-    def __str__(self):
-	return self.name
-
 
 class UserShortlistedCourse(models.Model):
-    user_id = models.IntegerField()
+    user_name = models.ForeignKey(User)
     class_number = models.IntegerField()
     priority = models.IntegerField(default=0)
 
     def __str__(self):
 	return "User: " + self.user_id + "Class Number:" + self.class_number
+		
+	
